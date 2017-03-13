d8948fdd0585d70d85d3df472cce6304e27c2b3c
devichappidi <devimeghana406@gmail.com>
2017-03-13 16:40:02 +0530

    added product details

--- productDetails.js
+++ productDetails.js
@@ -1,23 +1,41 @@
-angular.module("productDetails", [])
-    .controller("detailsController", function ($scope, $http, $stateParams, $state) {
-        var proinfo = JSON.parse($stateParams.details);
-        $scope.name = proinfo.name;
-        $scope.price = proinfo.price;
-        // $scope.proDetails = function (id) {
-        console.log("id" + proinfo.model);
+ var myModule = angular.module("productdetails",['ui.router'])
+               myModule.controller("productdetailsctrl",function($scope,$window,$state,$http){
+                   $scope.data = {}
+                   $http({
+                       url:"vehicles.json",
+                       method:"GET",
 
-        // })
-        $scope.cart = function () {
+                   })
+                   .then(function(response){
+                $scope.myProduct = response.data.products;
+                     console.log("hai");
+                    })
+                     $scope.buy = function(){
+                       $state.go("     ")
+                       console.log("hai");
+
+                   }
+                   $scope.back = function(){
+                    $state.go("hello");
+                   }
+                   $scope.add = function(){
+                       var count = count+1;
+                       return count;
+                   }
+
+               })
+               .config(function($stateProvider){
+                   $stateProvider
+                          .state("details",{
+                          url : '/details/:id',
+                          templateUrl : "productDetail.html",
+                          controller : "productdetailsctrl"
+                          });
+                   $stateProvider.state("hello",{
+                       url : '/hi',
+                       templateUrl : "hi.html",
+                       controller : "roductdetailsctrl"
+                   });
 
 
-        }
-        $scope.buy = function () {
-            //console.log("Details" + model);
-            $state.go("cartDetails"
-
-
-            );
-
-        }
-
-    });
+               })
